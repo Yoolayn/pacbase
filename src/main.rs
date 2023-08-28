ff29b19c5b1a533baa03e1de5b42787f080aaee2
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-28 22:44:05 +0200

    feat: set constants and modules to use

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::process::Command;
+use rusqlite::{Connection, Result};
+
+const DB_PATH: &str = "mydb.db";
+const CACHE_PATH: &str = "/store/cache";
+
 fn main() {
     println!("Hello, world!");
 }
